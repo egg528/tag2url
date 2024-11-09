7d518baaa83138a2c8551ca9f3bd26e99ab1b50e
egg528 <rnjsdntjr26@daum.net>
2024-11-10 01:40:28 +0900

    :triangular_flag_on_post: Member Model 및 기본 resource 생성

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :members
   get 'cat_motivation', to: 'cat_motivation#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ db/migrate/20241109163723_create_members.rb
@@ -0,0 +1,10 @@
+class CreateMembers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :members do |t|
+      t.string :access_key
+      t.string :nickname
+
+      t.timestamps
+    end
+  end
+end
