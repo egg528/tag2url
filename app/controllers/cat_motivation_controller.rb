4fc9c30db2b8ad8d4a7f91834ee898cabda50e67
egg528 <rnjsdntjr26@daum.net>
2024-11-10 17:16:44 +0900

    :triangular_flag_on_post: 초기 렌더링 시 선택 값과 명언도 Props로 내려주도록 수정

--- app/controllers/cat_motivation_controller.rb
+++ app/controllers/cat_motivation_controller.rb
@@ -5,14 +5,33 @@ class CatMotivationController < ApplicationController
   layout "default"
 
   def index
-    @props = { member_id: @member.id, member_nickname: @member.nickname }
+    select_quote_type = @member.selections.first&.quote_type
+
+    if select_quote_type.blank?
+      return @props = {
+        member_id: @member.id,
+        member_nickname: @member.nickname,
+        select_quote_type: '',
+        quote: '',
+      }
+    end
+
+    # TODO: 명언 선정 방식 수정 필요
+    quote = Quote.where(quote_type: today_select_type).order('RANDOM()').first
+
+    @props = {
+      member_id: @member.id,
+      member_nickname: @member.nickname,
+      select_quote_type: @member.selections.first&.quote_type,
+      quote: `#{quote.content}-#{quote.author}`
+    }
   end
 
   private
 
   def authenticate_member
     token = cookies[:access_token]
-    @member = Member.find_by(access_token: token)
+    @member = Member.includes(:selections).where(selections: {created_at: Date.today.all_day}).find_by(access_token: token)
 
     unless @member
       render json: { error: 'Unauthorized' }, status: :unauthorized
