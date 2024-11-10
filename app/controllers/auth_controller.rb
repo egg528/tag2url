7b54742f5bea97834c91ef14c3f19d95adf44e92
egg528 <rnjsdntjr26@daum.net>
2024-11-10 16:56:55 +0900

    :adhesive_bandage: access_token cookie 추가 시 만료 기간 추가

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,10 @@
 
 class AuthController < ApplicationController
   def authenticate
-    cookies[:access_token] = params[:access_token]
+    cookies[:access_token] = {
+      value: params[:access_token],
+      expires: 1.hour.from_now
+    }
 
     redirect_to "/cat_motivation"
   end
